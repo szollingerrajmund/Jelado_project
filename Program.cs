758736bd2bcc24edf80c284309f933b9ca02da4e
simonadamgyula <simon.adam.gyula@students.jedlik.eu>
2025-02-27 07:31:19 +0100

    állomanybol adatok beolvasasa

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,15 @@ namespace Jelado_proj
 {
     internal class Program
     {
+        static string allomanyNev = "jel.txt";
+        static List<Jelado> jeladok = new List<Jelado>();
+
         static void Main(string[] args)
         {
+            foreach (string sor in File.ReadAllLines(allomanyNev))
+            {
+                jeladok.Add(new Jelado(sor));
+            }
         }
     }
 }
